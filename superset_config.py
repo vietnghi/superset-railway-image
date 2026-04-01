c30d84927ee811af474ad32349ed01ce2b81c865
vietnghi <vietnghizs@gmail.com>
2026-04-01 23:33:14 +0700

    Refactor environment variable retrieval in superset_config

--- superset_config.py
+++ superset_config.py
@@ -4,6 +4,7 @@ import logging
 logger = logging.getLogger()
 
 PYTHONPATH = os.environ.get('PYTHONPATH', '')
+
 RATELIMIT_STORAGE_URI = os.environ.get('SUPERSET_CACHE_REDIS_URL', '')
 SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI', '')
 SUPERSET_CACHE_REDIS_URL = os.environ.get('SUPERSET_CACHE_REDIS_URL', '')
@@ -13,24 +14,24 @@ CACHE_CONFIG = {
     "CACHE_DEFAULT_TIMEOUT": 300,
     "CACHE_KEY_PREFIX": "superset_",
     "CACHE_REDIS_HOST": os.environ.get('REDIS_HOST', 'localhost'),
-    "CACHE_REDIS_PORT": int(os.environ.get('REDIS_PORT', 6379)),
+    "CACHE_REDIS_PORT": int(os.environ.get('REDIS_PORT', '6379')),
     "CACHE_REDIS_DB": 0,
     'CACHE_REDIS_URL': os.environ.get('REDIS_URL', '')
 }
 DATA_CACHE_CONFIG = CACHE_CONFIG
 
 class CeleryConfig(object):
-    BROKER_URL = f$SUPERSET_CACHE_REDIS_URL + "/1"
-    CELERY_IMPORTS= ("superset.sql_lab",)
-    CELERY_RESULT_BACKEND = f$SUPERSET_CACHE_REDIS_URL + "/0"
+    BROKER_URL = os.environ.get('SUPERSET_CACHE_REDIS_URL', '') + "/1"
+    CELERY_IMPORTS = ("superset.sql_lab",)
+    CELERY_RESULT_BACKEND = os.environ.get('SUPERSET_CACHE_REDIS_URL', '') + "/0"
     CELERY_ANNOTATIONS = {'tasks.add': {'rate_limit': '10/s'}}
 
 CELERY_CONFIG = CeleryConfig
 
-REDIS_HOST = $REDIS_HOST
-REDIS_PORT = $REDIS_PORT
+REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
+REDIS_PORT = int(os.environ.get('REDIS_PORT', '6379'))
 
-SUPERSET_ENV = $SUPERSET_ENV
-SUPERSET_LOAD_EXAMPLES = $SUPERSET_LOAD_EXAMPLES
-SUPERSET_SECRET_KEY = $SUPERSET_SECRET_KEY
-SUPERSET_PORT = $SUPERSET_PORT
+SUPERSET_ENV = os.environ.get('SUPERSET_ENV', 'development')
+SUPERSET_LOAD_EXAMPLES = os.environ.get('SUPERSET_LOAD_EXAMPLES', 'false').lower() == 'true'
+SUPERSET_SECRET_KEY = os.environ.get('SUPERSET_SECRET_KEY', 'dev-secret-key')
+SUPERSET_PORT = os.environ.get('SUPERSET_PORT'
